7e4fca44c48debcd9574b33e37eb8776b8813899
patrycja <gumis9049@gmail.com>
2021-01-10 16:23:33 +0100

    Fixed errors found by ESLint

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import styles from "./Card.scss";
-import {settings} from '../../data/dataStore';
+import styles from './Card.scss';
 
 class Card extends React.Component {
   state = {
