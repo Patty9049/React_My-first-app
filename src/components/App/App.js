8befc175a888b54f2c12151b22401ef6a7830980
patrycja <gumis9049@gmail.com>
2021-01-13 16:39:02 +0100

    Creating the general state of aplication. First elements moved to Redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './App.scss';
-// import List from '../List/List';
-// import {pageContents /*listData*/} from '../../data/dataStore';
-
+import List from '../List/List';
 
 class App extends React.Component {
   static propTypes = {
     title: PropTypes.node,
     subtitle: PropTypes.node,
+    lists: PropTypes.array,
   }
   render() {
+    const {lists, title, subtitle} = this.props;
     return (
       <div id='app'>
         <main className={styles.component}>
-          {/* <h1 className={styles.title}>{pageContents.title}</h1>
-          <h2 className={styles.subtitle}>{pageContents.subtitle}</h2> */}
-          {/* <List {...listData} /> */}
+          <h1 className={styles.title}>{title}</h1>
+          <h2 className={styles.subtitle}>{subtitle}</h2>
+          {/* < List {...listData}/> */}
+          {lists.map(listData => (
+            <List key={listData.id} {...listData} />
+          ))}
         </main>
       </div>
     );
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,18 +2,17 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column';
-import {settings, listData} from '../../data/dataStore';
-import Creator from '../Creator/Creator';
+// import Column from '../Column/Column';
+import {settings /* listData */} from '../../data/dataStore';
+// import Creator from '../Creator/Creator';
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  }
+  // state = {
+  //   columns: this.props.columns || [],
+  // }
   static propTypes = {
     title: PropTypes.node.isRequired,
     headerImgSrc: PropTypes.string,
-    headerImgAlt: PropTypes.string,
     description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.string,
@@ -21,41 +20,38 @@ class List extends React.Component {
   static defaultProps = {
     description: settings.defaultListDescription,
   }
-  data(){
-    console.log('this', this);
-    console.log('this.state', this.state);
-  }
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
+  // addColumn(title){
+  //   this.setState(state => (
+  //     {
+  //       columns: [
+  //         ...state.columns,
+  //         {
+  //           key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+  //           title,
+  //           icon: 'list-alt',
+  //           cards: [],
+  //         },
+  //       ],
+  //     }
+  //   ));
+  // }
 
   render() {
+    const {title, image, description} = this.props;
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} imgSrc={this.props.image} imgAlt={this.props.headerImgAlt}/>
+        <Hero titleText={title} imgSrc={image}/>
         <div className={styles.description}>
-          {this.props.description}
+          {description}
         </div>
-        <div className={styles.columns}>
+        {/* <div className={styles.columns}>
           {this.state.columns.map(({key, ...columnProps}) => (
             <Column {...listData} key={key} {...columnProps} />
           ))}
         </div>
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-        </div>
+        </div> */}
       </section>
     );
   }
