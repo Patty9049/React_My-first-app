41e3cfcd3165b2a6110f88b15ed89e304e5a23f4
patrycja <gumis9049@gmail.com>
2021-01-23 16:58:32 +0100

    Component Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './App.scss';
-import List from '../List/ListContainer';
-import Search from '../Search/SearchContainer';
+import Home from '../Home/HomeContainer';
+import Info from '../Info/Info';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import MainLayout from '../MainLayout/MainLayout';
 
-class App extends React.Component {
-  static propTypes = {
-    title: PropTypes.node,
-    subtitle: PropTypes.node,
-    lists: PropTypes.array,
-    changeSearchString: PropTypes.func,
-  }
-  render() {
-    const {lists, title, subtitle, changeSearchString} = this.props;
-    return (
-      <div id='app'>
-        <main className={styles.component}>
-          <h1 className={styles.title}>{title}</h1>
-          <h2 className={styles.subtitle}>{subtitle}</h2>
-          <Search action={changeSearchString}/>
-          {/* < List {...listData}/> */}
-          {lists.map(listData => (
-            <List key={listData.id} {...listData} />
-          ))}
-        </main>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <MainLayout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/info' component={Info} />
+      </Switch>
+    </MainLayout>
+  </BrowserRouter>
+);
 
 export default App;
--- src/components/Home/HomeContainer.js
+++ src/components/Home/HomeContainer.js
@@ -1,5 +1,5 @@
 import {connect} from 'react-redux';
-import App from './App';
+import Home from './Home';
 import {createAction_changeSearchString} from '../../redux/searchStringRedux';
 
 const mapStateToProps = state => ({
@@ -16,4 +16,4 @@ const mapDispatchToProps = (dispatch) => ({
   })),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
